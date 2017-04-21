f9d9de8e96b66b0454c104c86459e169c031116e
martimutso <marti.mutso@gmail.com>
2017-04-21 14:01:01 +0300

    Update NaideMain.hs

--- NaideMain.hs
+++ NaideMain.hs
@@ -3,11 +3,13 @@ module NaideMain where
 import NaideDataSource
 import Haxl.Core
 
-main :: IO ()
-main = do
+paring :: IO ()
+paring = do
     myEnv <- initEnv initialState ()
     r <- runHaxl myEnv (dataFetch Vanus)
+    r1 <- runHaxl myEnv (dataFetch Vanus)
     print r
+    print r1
 
 initialState :: StateStore
 initialState = stateSet NoState stateEmpty
