73a1a4bceed0a965cd879c2cf2cde405d9b78e36
martimutso <marti.mutso@gmail.com>
2017-04-21 18:04:48 +0300

    Update StoreHaxl.hs

--- StoreHaxl.hs
+++ StoreHaxl.hs
@@ -6,60 +6,10 @@ import NaideDataSource
 import Haxl.Core
 
 getTrackAlbumGenre :: GenHaxl () Int -> GenHaxl () Int -> GenHaxl () [String]
-getTrackAlbumGenre albumId artist = join (dataFetch <$> (GetTrackByAlbumGenre <$> albumId <*> artist))
+getTrackAlbumGenre albumId artist = dataFetch =<< GetTrackByAlbumGenre <$> albumId <*> artist
 
-getAlbumId :: GenHaxl () String -> GenHaxl () Int
-getAlbumId albumName = join (dataFetch <$> (GetAlbumId <$> albumName))
+getAlbumId :: String -> GenHaxl () Int
+getAlbumId albumName = dataFetch =<< GetAlbumId <$> (pure albumName)
 
-getGenre :: GenHaxl () String -> GenHaxl () Int
-getGenre genreName = join (dataFetch <$> (GetGenreId <$> genreName))
-
-{-
-getArtist :: GenHaxl () Int -> GenHaxl () String
-getArtist artistId = join (dataFetch <$> (GetArtistById <$> artistId))
-
-getArtistId :: GenHaxl () String -> GenHaxl () Int
-getArtistId artistName = join (dataFetch <$> (GetArtistByName <$> artistName))
-
-getAlbum :: GenHaxl () Int -> GenHaxl () String
-getAlbum albumId = join (dataFetch <$> (GetAlbumById <$> albumId))
-
-getArtistAlbums :: Int -> GenHaxl () [String]
-getArtistAlbums artistId = dataFetch (GetAlbumsByArtistId artistId)
-
-getTrack :: Int -> GenHaxl () String
-getTrack trackId = dataFetch (GetTrackById trackId)
-
-getTracks :: Int -> GenHaxl () [String]
-getTracks albumId = dataFetch (GetAlbumTracks albumId)
-
-getTracksArtist :: Int -> GenHaxl () [String]
-getTracksArtist artistId = dataFetch (GetArtistTracks artistId)
-
-getTrackType :: Int -> GenHaxl () String
-getTrackType trackId = dataFetch (GetTrackMediatype trackId)
-
-getArtist2 :: Int -> GenHaxl () String
-getArtist2 artistId = dataFetch (GetArtistById2 artistId)
-
-getArtistId2 :: String -> GenHaxl () Int
-getArtistId2 artistName = dataFetch (GetArtistByName2 artistName)
-
-getAlbum2 :: Int -> GenHaxl () String
-getAlbum2 albumId = dataFetch (GetAlbumById2 albumId)
-
-getArtistAlbums2 :: Int -> GenHaxl () [String]
-getArtistAlbums2 artistId = dataFetch (GetAlbumsByArtistId2 artistId)
-
-getTrack2 :: Int -> GenHaxl () String
-getTrack2 trackId = dataFetch (GetTrackById2 trackId)
-
-getTracks2 :: Int -> GenHaxl () [String]
-getTracks2 albumId = dataFetch (GetAlbumTracks2 albumId)
-
-getTracksArtist2 :: Int -> GenHaxl () [String]
-getTracksArtist2 artistId = dataFetch (GetArtistTracks2 artistId)
-
-getTrackType2 :: Int -> GenHaxl () String
-getTrackType2 trackId = dataFetch (GetTrackMediatype2 trackId)
--}
+getGenre :: String -> GenHaxl () Int
+getGenre genreName = dataFetch =<< GetGenreId <$> (pure genreName)
