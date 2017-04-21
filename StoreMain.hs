0a1eef944ec7487b2d8e7feeb5b6d54dc7fa3f9f
martimutso <marti.mutso@gmail.com>
2017-04-21 14:00:40 +0300

    Update StoreMain.hs

--- StoreMain.hs
+++ StoreMain.hs
@@ -2,6 +2,7 @@ module StoreMain where
 
 import StoreDataSource
 import StoreHaxl
+import NaideDataSource
 import Control.Monad
 import Database.HDBC.Sqlite3
 import Database.HDBC
@@ -10,27 +11,12 @@ import Data.Typeable
 import Text.Printf
 import Haxl.Core
 
-main :: IO ()
-main = do
+
+paring :: IO ()
+paring = do
     connState <- initConnState
-    --connState2 <- initConnState2
-    myEnv <- initEnv (stateSet connState stateEmpty) ()
-    --myEnv2 <- initEnv (stateSet connState2 stateEmpty) ()
+    myEnv <- initEnv (stateSet NoState $ stateSet connState $ stateEmpty) ()
     r <- runHaxl myEnv $ (getTrackAlbumGenre (getAlbumId (pure "For Those About To Rock We Salute You")) (getGenre (pure "Rock")))
-        
-    putStr ((show r) ++ "\n")
-    --g <- runHaxl myEnv (getAlbum 1)
-    --putStr r
-    --putStr g
-    {-f <- runHaxl myEnv (getArtistAlbums 1)
-    l <- runHaxl myEnv (getTrack 1)
-    k <- runHaxl myEnv (getTracks 1)
-    m <- runHaxl myEnv (getTracksArtist 1)
-    n <- runHaxl myEnv (getTrackType 1)
-    putStr r
-    putStr g
-    putStr ((show f) ++ "\n")
-    putStr l
-    putStr ((show k) ++ "\n")
-    putStr ((show m) ++ "\n")
-    putStr n-}
+    --r1 <- runHaxl myEnv $ (dataFetch Vanus)
+    putStrLn ((show r) ++ "\n")
+    --putStrLn ((show r) ++ "\n" ++ (show r1))
